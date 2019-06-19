96e8be75ef7fe7a4febefa502645479c7812ed5a
leventalce <51987558+leventalce@users.noreply.github.com>
2019-06-19 10:30:24 +0200

    Add files via upload

---
+++ FoodPlan/app/src/main/java/com/example/foodplan/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.foodplan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        String ApiUrl = "http://myresume.dk/SanbotWebsite/php/API/MadplanAPI.php?year=" + jjj + "&week=" + jjj;
+
+        JsonObjectRequest EventRequest = new JsonObjectRequest(Request.Method.GET, ApiUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("Madplan");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        mQueue.add(EventRequest);
+    }
+}
